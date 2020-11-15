074c3d9324801ac4fef464135c1d72b4a5bd2e69
PatrikCesnek <pcesnek290@gmail.com>
2020-11-15 10:22:54 +0100

    Initial Commit

---
+++ SnowSeeker/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  SnowSeeker
+//
+//  Created by Patrik Cesnek on 11/11/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello world")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SnowSeeker/SnowSeekerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnowSeekerApp.swift
+//  SnowSeeker
+//
+//  Created by Patrik Cesnek on 11/11/2020.
+//
+
+import SwiftUI
+
+@main
+struct SnowSeekerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
